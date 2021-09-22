1af7a3a1ce31c7f51bb3476c005487ec02489f0c
vinisha2512 <adappavinisha@gmail.com>
2021-09-22 12:38:16 +0530

    First Commit

---
+++ Medistop/wsgi.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+"""
+WSGI config for Medistop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medistop.settings')
+
+# adjust as appropriate
+project_folder = os.path.expanduser('~/MediStop/Medistop')
+load_dotenv(os.path.join(project_folder, '.env'))
+print(os.path.join(project_folder, '.env'))
+
+application = get_wsgi_application()
---
+++ user/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import os
+# Create your views here.
+
+config = {
+    "apiKey": os.getenv("apiKey"),
+    "authDomain": os.getenv("authDomain"),
+    "databaseURL": os.getenv("databaseURL"),
+    "projectId": os.getenv("projectId"),
+    "storageBucket": os.getenv("storageBucket"),
+    "messagingSenderId": os.getenv("messagingSenderId"),
+    "appId": os.getenv("appId"),
+    "measurementId": os.getenv("measurementId"),
+}
