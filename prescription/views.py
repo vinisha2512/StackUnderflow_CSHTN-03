2ba408b3b6b06df8cdeb0cab38753ccfd647215d
Vinisha <adappavinisha@gmail.com>
2021-09-24 18:52:55 +0530

    Temp commit

--- prescription/views.py
+++ prescription/views.py
@@ -6,7 +6,25 @@ from dotenv import load_dotenv
 load_dotenv('\.env')
 from django.http.response import HttpResponse
 import pandas as pd
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import default_storage
+
+from PIL import Image
+import os, io
+from google.cloud import vision_v1
+import difflib
+import csv
+from google.cloud.vision_v1 import types
+import pathlib
+from pdf2image import convert_from_path
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+
+# importing get_template from loader
+from django.template.loader import get_template
+
+# import render_to_pdf from util.py
+from .utils import render_to_pdf
 
 # Create your views here.
 # print(os.getenv("apiKey"))
@@ -26,86 +44,108 @@ authe = firebase.auth()
 database = firebase.database()
 storage = firebase.storage()
 
-
 def f1(request):
     if request.method =='POST':
-        name=request.POST.get('name')
-        print(name)
-        return HttpResponse('')
-
-def inc_cart_qty(request):
-    if request.method =='POST':
+        # print(name,quan)
+        hell="yes"
         UID= request.session['uid']
-        med_name=request.POST.get('meds')
-        quantity=request.POST.get('no_units')
-        
-        database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":quantity})
-                                                                        # quantity lena chahiye yaa +1 ye doubt hai 
+        med_name=request.POST.get('name')
+        quantity=request.POST.get('quantity')
+        print("Hola", UID, quantity)
+        try:
+            value_in_cart = int(dict(database.child("Users").child(UID).child("Cart").child(med_name).get().val())["Quantity"]) + quantity
+            database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":value_in_cart})
+        except:
 
+            database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":quantity})
+        # database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":quantity})
+                                                                        # quantity lena chahiye yaa +1 ye doubt hai 
+        quan_left = int(dict(database.child("Medicine").child(med_name).get().val())["Quantity"])
         noofmeets = int(dict(database.child("Medicine").child(med_name).get().val())["Temp"]) + 1
+        # value_in_cart = int(dict(database.child("Users").child(UID).child("Cart").child(med_name).get().val())["Quantity"]) + 1
+        
+        if(quan_left-noofmeets<=0):
+            hell="no"
+
+        # database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":value_in_cart})
         database.child("Medicine").child(med_name).update({"Temp": noofmeets})
 
-        return HttpResponse('')
+        return HttpResponse(hell)
 
-def change_invent_qty(request):
+
+
+def f2(request):
     if request.method =='POST':
-        UID= request.session['uid']
-        med_name=request.POST.get('meds')
-        quantity=request.POST.get('no_units')
+        # print(name,quan)
+        hell="yes"
+        UID= "Rsz5MPwnYhbpNV6D42qorchrvBH3"
+        med_name=request.POST.get('name')
+        # quantity=request.POST.get('quantity')
         
         # database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":quantity})
                                                                         # quantity lena chahiye yaa +1 ye doubt hai 
-
-        # noofmeets = int(dict(database.child("Medicine").child(med_name).get().val())["Temp"]) + 1
-        database.child("Medicine").child(med_name).update({"Quantity": quantity})
-
-        return HttpResponse('')
-
-
-
-def product(request):
-
-    medname=request.POST.get("medicine_name")
-    # data1=[]
-    # classa = "Allegra 180mg Tablet"
-    # df=pd.read_csv('New_Sample.csv')
-    # newdf=df.query('Medicine_Name == "{0}"'.format(classa))
-    # print(newdf)
-    # data_array=newdf.values[0]
-    # print(data_array)
-
-
-    return render(request,"login.html")
-
-def read_presc(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
+        # quan_left = int(dict(database.child("Medicine").child(med_name).get().val())["Quantity"])
+        noofmeets = int(dict(database.child("Medicine").child(med_name).get().val())["Temp"]) - 1
+        value_in_cart = int(dict(database.child("Users").child(UID).child("Cart").child(med_name).get().val())["Quantity"]) - 1
         
 
-    # return render(request, 'cart.html')
 
-        from PIL import Image
-        import os, io
-        from google.cloud import vision_v1
-        import difflib
-        import csv
-        from google.cloud.vision_v1 import types
-        import pathlib
+        database.child("Users").child(UID).child("Cart").child(med_name).set({"Quantity":value_in_cart})
+        database.child("Medicine").child(med_name).update({"Temp": noofmeets})
 
-        #getting file from front end
-        imagename='no3.png'
+        return HttpResponse("")
+        # name=request.POST.get('name')
+        # quan=request.POST.get('quantity')
+        # print(name,quan)
+        # hell = "yes"
+        # return HttpResponse(hell)
+
+def delete(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        uid=request.POST.get('uid')
+        database.child("Users").child(uid).child("Cart").child(name).remove()
+        # quan=request.POST.get('quantity')
+        print(name)
+        print("delete row: ",name)
+        hell = "yes"
+        return HttpResponse(hell)
+
+
+def upload(request):
+    return render(request, "upload-prescription.html")
+
+def read_presc(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        imagename = file.name
+        ext = pathlib.Path(imagename).suffix
+
+        file_save = default_storage.save(imagename, file)
+        ref = storage.child("Prescription").child(request.session["uid"]).put("media/" + imagename)
+        
+
+        def function():
+            try:
+                downloadurl = storage.child("Prescripton").child(request.session["uid"]).get_url(None)
+                return downloadurl
+            except:
+                downloadurl= ""
+                return downloadurl
         
         ext = pathlib.Path(imagename).suffix
 
-        #Reading prescription photo for dr and date tag
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ascendant-choir-326117-f6d97fe48d5c.json'
+        # if ext==".pdf":
+        #     images = convert_from_path('example.pdf')
+        #     for i in range(len(images)):
+        #         images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+        # #Reading prescription photo for dr and date tag
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ascendant-choir-326117-f6d97fe48d5c.json' #path
         client = vision_v1.ImageAnnotatorClient()
 
-        FOLDER_PATH = r''
-        # IMAGE_FILE = './fromfrontend/'+imagename
+        FOLDER_PATH = r'media/' #prescription path
+        # # IMAGE_FILE = './fromfrontend/'+imagename
         IMAGE_FILE = imagename
         FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
 
@@ -120,25 +160,21 @@ def read_presc(request):
         print(docText)
 
 
-        text_array0 = docText.split();
+        text_array0 = docText.split()
 
 
         #Cropping uploaded prescription
         # cropped_name = './cut/crop'+ext
-        cropped_name="crop"+ext
+        cropped_name="crop"+ext   
         # im = Image.open('./fromfrontend/'+imagename)
-        im = Image.open(imagename)
+        im = Image.open('media/'+imagename)
 
         height,width=im.size
 
-        im.crop((0, height*.30, width, height+20)).save(cropped_name, quality=95)
+        im.crop((0, height*.30, width, height+20)).save("media/"+cropped_name, quality=95) #cropped name can be path as well
 
 
-        #reading cropped prescription for meds
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ascendant-choir-326117-f6d97fe48d5c.json'
-        client = vision_v1.ImageAnnotatorClient()
-
-        FOLDER_PATH = r''
+        FOLDER_PATH = r'media/' #folderpath
         IMAGE_FILE = cropped_name
         FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
 
@@ -151,7 +187,7 @@ def read_presc(request):
         docText2 = response.full_text_annotation.text
         print(docText2)
 
-        text_array = docText2.split();
+        text_array = docText2.split()
         # print(text_array)
         try:
             n=text_array0.index("Dr")
@@ -168,52 +204,113 @@ def read_presc(request):
             res2="No date found."
             print(res2)
 
-        # UID=request.session['UID']
-        UID="Rsz5MPwnYhbpNV6D42qorchrvBH3"
+        UID=request.session['uid']
         data= pd.read_csv("names.csv")
         name_of_meds=data['Medicine_Name'].tolist()
-        print(text_array)
 
-
-        meds=[]
-        meds_details=[]
-
+        medi, sell_type, price, lqty, qty = [], [], [], [], []
         for i in text_array:
             if len(i)<3:
                 continue
             else:
                 value=difflib.get_close_matches(i, name_of_meds,1, 0.8)
-
-                # print(name_of_meds)
                 print(value)
                 for j in value:
                     if j !=None or j !="":
-                        print(name_of_meds.index(j))
                         indexincsv=name_of_meds.index(j)
-                        dic={
-                            j:indexincsv
-                        }
-                        meds.append(dic.copy())
                         count=0
                         with open('New_sample.csv', 'r') as f:
                             csv_reader = csv.reader(f)
                             for row in csv_reader:
                                 if count ==indexincsv+1:  
-                                    info=[row[0],row[2],row[5]] 
-                                    meds_details.append(info)
+                                    quan_left = int(dict(database.child("Medicine").child(row[0]).get().val())["Quantity"])
+                                    medi.append(row[0])
+                                    sell_type.append(row[2])
+                                    price.append(row[5][4:])
+                                    lqty.append(quan_left)
+                                    qty.append(1)
+                                    # database.child("Users").child(UID).child("Cart").child(row[0]).set({"Quantity":"1"})
+                                    # # database.child("Medicines").child(row[0]).child("temp").
 
-                                    database.child("Users").child(UID).child("Cart").child(row[0]).set({"Quantity":"1"})
-                                    # database.child("Medicines").child(row[0]).child("temp").
-
-                                    noofmeets = int(dict(database.child("Medicine").child(row[0]).get().val())["Temp"]) + 1
-                                    database.child("Medicine").child(row[0]).update({"Temp": noofmeets})
-
+                                    # noofmeets = int(dict(database.child("Medicine").child(row[0]).get().val())["Temp"]) + 1
+                                    # database.child("Medicine").child(row[0]).update({"Temp": noofmeets})
                                 elif count>indexincsv:
                                     break
                                 count=count+1
-
-        print(meds)
-        print(meds_details)
-
-        return render(request,'homepage.html',{"data":meds_details})
+        delete = default_storage.delete(imagename)
+        delete = default_storage.delete(cropped_name)
+        combilis = zip(medi, sell_type, price, qty, lqty)
+        cart_dct = {"medi": medi, "sell_type": sell_type, "price": price, "qty":qty}
+        request.session["cart"] = cart_dct
+        presc_link = storage.child("Prescripton").child(request.session["uid"]).get_url(None)
+        return render(request,'cart.html',{"data":combilis, "presc_link": presc_link})
     return render(request,'upload-prescription.html')
+    
+def cart(request):
+    UID=request.session['uid']
+    cart = list(database.child("Users").child(UID).child("Cart").shallow().get().val())
+    qty_list = dict(database.child("Users").child(UID).child("Cart").get().val())
+    data= pd.read_csv("New_sample.csv")
+    name_of_meds=data['Medicine_Name'].tolist()
+    medi, sell_type, price, lqty, qty, presc_req = [], [], [], [], [], []
+    for j in cart:
+        if j !=None or j !="":
+            indexincsv=name_of_meds.index(j)
+            count=0
+            with open('New_sample.csv', 'r') as f:
+                csv_reader = csv.reader(f)
+                for row in csv_reader:
+                    if count ==indexincsv+1:  
+                        quan_left = int(dict(database.child("Medicine").child(row[0]).get().val())["Quantity"])
+                        medi.append(row[0])
+                        presc_req.append(row[1])
+                        sell_type.append(row[2])
+                        price.append(row[5][4:])
+                        qty_left=int(dict(database.child("Medicine").child(row[0]).get().val())["Quantity"])
+                        temp=int(dict(database.child("Medicine").child(row[0]).get().val())["Temp"])
+                        lqty.append(min(qty_left-temp,5))
+                        qty.append(qty_list[j]["Quantity"])
+                    elif count>indexincsv:
+                        break
+                    count=count+1
+    if "Prescription Required" in presc_req:
+        prescreq = True
+    combilis = zip(medi, sell_type, price, qty, lqty, presc_req)
+    return render(request,"cart.html", {"presc_req": prescreq, "data": combilis, "manual": True})
+
+def placeOrder(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        address=request.POST.get('address')
+        phoneNo = request.POST.get("phoneNo")
+        mode = request.POST.get('mode')
+        presc_req = request.POST.get("presc_req")
+        total = request.POST.get("total")
+        email = request.POST.get("email")
+        from datetime import date
+        today = date.today()
+        d1 = today.strftime("%d_%m_%Y")
+        # print(name, address, phoneNo, mode, presc_req)
+        if mode=="manual":
+            if (presc_req):
+                file = request.FILES['FileUpload']
+                print(file)
+                cart = dict(database.child("Users").child(request.session["uid"]).child("Cart").child().get().val())
+                print(cart)
+                data = {"Address":address, "Name":name, "FileLink": "None","Payment": "COD", "Status": "Not Verified", "Phone": phoneNo, "Scan": 0, "Total": total, "Email": email, "Date": d1}
+                database.child("Users").child(request.session["uid"]).child("Orders").child("details").push(data)
+            else:
+                data = {"Address":address, "Name":name, "Payment": "COD", "Status": "Not Verified", "Phone": phoneNo, "Scan": 0, "Total": total, "FileLink": "None", "Email":email, "Date": d1}
+                x = database.child("Users").child(request.session["uid"]).child("Orders").push(data)
+                print(x)
+                
+                dict1 = {"data": "x", "data2": ["1", "2", "3"], "data3": 5}
+                pdf = render_to_pdf('invoice.html', dict1)
+                ref = storage.child("Invoice").child(request.session["uid"]+"_"+x["name"]).put("prescription.pdf")
+
+                invoicelink = storage.child("Invoice").child(request.session["uid"]+"_"+x["name"]).get_url(None)
+                data = {"InvoiceLink" : invoicelink, "UID":request.session["uid"]}
+                database.child("AdminV").child("Veri").child(x["name"]).set(data)
+        else:
+            presc_link = request.POST.get("presc_link")
+        return render(request, "Home.html")
--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ import pyrebase
 from django.shortcuts import render, redirect
 from dotenv import load_dotenv
 load_dotenv('\.env')
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 # print(os.getenv("apiKey"))
@@ -24,6 +24,16 @@ authe = firebase.auth()
 database = firebase.database()
 storage = firebase.storage()
 
+
+def myuser_login_required(f):
+    def wrap(request, *args, **kwargs):
+        if 'member_id' not in request.session.keys():
+            return HttpResponseRedirect("/login")
+        return f(request, *args, **kwargs)
+    wrap._doc_ = f._doc_
+    wrap._name_ = f._name_
+    return wrap
+
 def xyz(request):
     return render(request, "home.html")
 
@@ -32,30 +42,28 @@ def retprod(request):
     data = {"meds":"_".join(meds)}
     return render(request, "index.html", data)
 
-def homepage(request):
-    return render(request, "trial.html")
 
+def homepage(request):
+    meds = list(database.child("Medicine").shallow().get().val())
+    data = {"meds":"_".join(meds)}
+    return render(request, "Home.html", data)
 
 def login(request):
-    print("x")
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
-        
+        print(email, password)
         try:
             user = firebase.auth().sign_in_with_email_and_password(email, password)
             uid=user['localId']
             request.session['uid']=uid
-            
-
+            request.session['loggedin'] = True
         except:
             message = "Invalid ID or Password"
             print("Error email")
-            return render(request, "login.html")
-        
-        # print(userinfo)
-        return redirect("homepage.html")
-    return render(request, "login.html")
+            return render(request, "Home.html") #error message dikhana padega
+        return redirect(homepage)
+    return render(request, "Home.html")
 
 
 
@@ -63,9 +71,9 @@ def signup(request):
     if request.method == "POST":
         email = request.POST.get("email")
         phonenumber = request.POST.get("phonenumber")
+        print(" ", phonenumber)
         name = request.POST.get("name")
         password = request.POST.get("password")
-
         try:
             user = authe.create_user_with_email_and_password(email, password)
             uid = user['localId'] 
@@ -76,20 +84,16 @@ def signup(request):
                 "Phone":phonenumber,
                 "Address":"-"
             }
+
             request.session['uid']=uid
+            request.session['loggedin'] = True
             database.child("Users").child(uid).child("Details").child("1").set(data)
-
-
-            return render(request,"homepage.html")       
+            return render(request,"Home.html")       
         except:
             message = "Unsuccessful in signing up, try again"
             print("Error email")
-            return render(request, "login.html")
-        
+            return render(request, "Home.html")  #error message dikhana padega
         # uid = user['localId']
-    
-       
-
         # print(userinfo)
         return redirect("dashboard")
     return render(request, "signup.html")
@@ -98,9 +102,80 @@ def signup(request):
 def reset(request):
     email = request.POST.get("email")
     authe.send_password_reset_email(email)
-    return render(request, "login.html")
+    return render(request, "Home.html")
+
+def med_class(request,category):
+    # fetching info for a therapeutic class
+    import pandas as pd
+    data1=[]
+    classa = category
+    col_list = ["Medicine_Name","Type_of_Sell","MRP","Therapeutic_Class","Url"]
+
+    df=pd.read_csv('New_Sample.csv',usecols=col_list)
+    newdf=df.query('Therapeutic_Class == "{0}"'.format(classa))
+    sending=newdf.values.tolist()
+    return render(request,"SeeAll.html",{"data1":sending[:5], "data2":sending[5:10], "data3":sending[10:15], "data4":sending[15:20], "class":category})
+
+def med_deets(request,MEDNAME):
+    import pandas as pd
+    data1=[]
+    classa = MEDNAME
+    df=pd.read_csv('New_Sample.csv')
+    newdf=df.query('Medicine_Name == "{0}"'.format(classa))
+
+    data_array=newdf.values.tolist()
+    
+    values=data_array[0]
+    name=values[0]
+    presc=values[1]
+    if(presc=="N"):
+        presc="No prescription required"
+    typeofsell=values[2]
+    manu=values[3]
+    salt=values[4].split('+')
+
+
+    mrp=values[5]
+    uses=values[6].split(',')
+    uses.pop()
+    alter=values[7].split(',')
+    alter.pop()
+    side=values[8].split(',')
+    side.pop()
+    howto=values[9].split('.')
+    howto.pop()
+    chemical=values[10]
+    habit=values[11]
+    thera=values[12]
+    action=values[13]
+    url=values[14]
+    med_name = name
+    name = name.replace(".", ",")
+    qty_left=int(dict(database.child("Medicine").child(name).get().val())["Quantity"])
+    temp=int(dict(database.child("Medicine").child(name).get().val())["Temp"])
+    qty_left=min(qty_left-temp,5)
+    
+    return render(request,"index__1.html",{"name":med_name,"pres":presc,"qty_left":qty_left,"typeofsell":typeofsell,"manu":manu,"salt":salt,"mrp":mrp,"uses":uses,"alter":alter,"side":side,"howto":howto,"chem":chemical,"habbit":habit,"thera":thera,"action":action,"url":url})
+
+def addtocart(request):
+    medname=request.POST.get('name')
+    print(medname)
+    number=int(request.POST.get('quantity'))
+    UID= "Rsz5MPwnYhbpNV6D42qorchrvBH3"
+    # quan_left = int(dict(database.child("Medicine").child(med_name).get().val())["Quantity"])
+    noofmeets = int(dict(database.child("Medicine").child(medname).get().val())["Temp"]) + number
+    try:
+        value_in_cart = int(dict(database.child("Users").child(UID).child("Cart").child(medname).get().val())["Quantity"]) + number
+        database.child("Users").child(UID).child("Cart").child(medname).set({"Quantity":value_in_cart})
+
+    except:
+
+        database.child("Users").child(UID).child("Cart").child(medname).set({"Quantity":number})
+    database.child("Medicine").child(medname).update({"Temp": noofmeets})
 
 
 
-
-
+    qty_left=int(dict(database.child("Medicine").child(medname).get().val())["Quantity"])
+    temp=int(dict(database.child("Medicine").child(medname).get().val())["Temp"])
+    qty_left=min(qty_left-temp,5)
+    return HttpResponse(qty_left)
